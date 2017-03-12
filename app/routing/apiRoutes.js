39fb1b7af1c6c0726596976990f215beafa66916
Ace ARK <ace@Akhilas-MacBook-Pro.local>
2017-03-12 01:28:53 -0500

    Adding feature of selecting epic type from which to compare.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,15 +15,21 @@ module.exports = function(app) {
 		var compatibleSpirit = {};
 		var smallestDeviation = 0;
 		var userStats = req.body.stats;
+
+		var listItemToCompare = friendsData.filter(item => item.type === req.body.type);
 		
-		for(var i=0; i<friendsData.length; i++) {
+		for(var i=0; i<listItemToCompare.length; i++) {
+			// console.log("Character data: " + friendsData);
+			
+			console.log("Line 22 Current character: " + listItemToCompare[i].name);
 			// Array containing differences between two arrays
 			var differenceArray = [];
 			// Sum of differences between two arrays
 			var sumOfDifferences = 0;
+			// console.log("Line 29 compatible spirit before compare: " + compatibleSpirit);
 			// Store current object's stats 
-			var currentFriendStats = friendsData[i].stats;
-
+			var currentFriendStats = listItemToCompare[i].stats;
+			console.log("Line 29 current Friend stats: " + currentFriendStats);
 			// Perform test
 			calculateDifferences(currentFriendStats, userStats);
 
@@ -32,14 +38,18 @@ module.exports = function(app) {
 				if(sumOfDifferences < smallestDeviation) {
 					// Assign that to parent variable
 					smallestDeviation = sumOfDifferences;
+					console.log("Line 38 Current deviation: " + smallestDeviation);
 					// Assign this person as compatible spirit
-					compatibleSpirit = friendsData[i];
+					compatibleSpirit = listItemToCompare[i];
+					console.log("Line 41 Current compatible: " + compatibleSpirit);
 				}
 			}else {
 				// Assign current sum to parent variable
 				smallestDeviation = sumOfDifferences;
+				console.log("Line 46 Current deviation: " + smallestDeviation);
 				// Current friend data to compatible spirit
-				compatibleSpirit = friendsData[i];
+				compatibleSpirit = listItemToCompare[i];
+				console.log("Line 49 Current compatible: " + compatibleSpirit);
 			}
 
 			// Calculate the difference array
@@ -48,8 +58,10 @@ module.exports = function(app) {
 				differenceArray = arrayOne.map(function(item, i) {
 					return Math.abs(item - arrayTwo[i]);
 				});
+				console.log("Line 61 Calculated difference array: " + differenceArray);
 				// Get sum of differences using the calculated array
 				sumOfDifferences = getSumOfDifferences(differenceArray);
+				console.log("Line 64 Sum of differences: " + sumOfDifferences);
 			}
 
 			// Get the sum of differences 
@@ -58,6 +70,7 @@ module.exports = function(app) {
 			}
 		}
 		// Send most compatible person after calculation
+		console.log("Data to be sent: " + JSON.stringify(compatibleSpirit));
 		res.json(compatibleSpirit);
 		// Push the new user into friendsData
 		friendsData.push(req.body);
